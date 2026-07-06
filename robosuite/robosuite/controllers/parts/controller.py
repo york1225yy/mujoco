7fba442a1ccc1a1115e7599669fecf5be1d44802
york1225yy <yy697098@gmail.com>
2026-07-06 14:03:57 +0800

    Fix mj_fullM API compatibility with MuJoCo 3.x
    MuJoCo 3.x changed mj_fullM signature from (model, dst, qM) to (model, data, dst).
    Updated controller.py to use new API.

--- robosuite/robosuite/controllers/parts/controller.py
+++ robosuite/robosuite/controllers/parts/controller.py
@@ -224,7 +224,7 @@ class Controller(object, metaclass=abc.ABCMeta):
             self.joint_vel = np.array(self.sim.data.qvel[self.qvel_index])
 
             mass_matrix = np.ndarray(shape=(self.sim.model.nv, self.sim.model.nv), dtype=np.float64, order="C")
-            mujoco.mj_fullM(self.sim.model._model, mass_matrix, self.sim.data.qM)
+            mujoco.mj_fullM(self.sim.model._model, self.sim.data._data, mass_matrix)
             mass_matrix = np.reshape(mass_matrix, (len(self.sim.data.qvel), len(self.sim.data.qvel)))
             self.mass_matrix = mass_matrix[self.qvel_index, :][:, self.qvel_index]
 
